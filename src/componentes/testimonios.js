c0741bdf77e3747db02435493f895f989c14a07c
Daniel medina <danielmedina7778@gmail.com>
2022-07-18 14:48:13 -0500

    trabajo terminado de la prueba 1

---
+++ src/componentes/testimonios.js
@@ -0,0 +1,25 @@
+import {testimonio} from "../stylesheets/testimonio.css"
+
+
+
+export default function TestimonioPersonas(props){
+
+  return(
+    <div className="contenedor-testimonios">
+      <img 
+        className="imagen-testimonio"
+        src={require(`../img/imagen-testimonio-${props.nombreImg}.png`)}
+        alt= {`foto de ${props.nombre}`}
+        title={`${props.nombre}`}
+      />
+      <div className="contenedor-texto">
+        <h3 className="nombre-testimonio">{props.nombre} in {props.ciudad}</h3>
+        <p className="empresa-testimonio">{props.carrera} at {props.empresa}</p>
+        <p className="texto-testimonio">"{props.testimonio}"</p>
+      </div>
+
+    </div>
+
+  )
+
+}
